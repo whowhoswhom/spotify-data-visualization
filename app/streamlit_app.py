39dff11c0b38aebfb662c8f37d8b24d46f9fb0a5
willlobb <welobb01@louisville.edu>
2025-02-21 19:28:42 -0500

    add base root render

---
+++ app/streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def run_app():
+    st.title("My Spotify Wrapped Clone")
+    st.write("This is rendered on the root page of the app.")
+
+# Display user data, charts, etc.
+# ...
