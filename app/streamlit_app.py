9c6cca1b42efc49bbc79a6acf6d47a6bde7d97a5
LDFINA01 <142958266+LDFINA01@users.noreply.github.com>
2025-02-21 19:48:05 -0500

    add new file struct

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 
-def run_app():
-    st.title("My Spotify Wrapped Clone")
-    st.write("This is rendered on the root page of the app.")
+st.title("My Spotify Wrapped Clone")
+st.write("This is rendered on the root page of the app.")
 
 # Display user data, charts, etc.
 # ...
